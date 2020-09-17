98f2edcdf67c9b8bed982f26ef00da6f68ce5ba3
Jordy Shryock <jordy1611@gmail.com>
2020-09-16 20:29:56 -0700

    Set dates in app state

--- src/App/App.js
+++ src/App/App.js
@@ -17,31 +17,14 @@ class App extends Component {
     }
   }
 
-createDateData() {
-  const date = new Date()
-  const today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()
-  const prevWeek = getPreviousWeek(new Date())
-  this.setState({ todaysDate: today, prevWeekDates: prevWeek })
-  // const day1 = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() - 1
-  // const date = new Date()
-  // const today = new Date(date.getFullYear(), date.getMonth(), date.getDate() - )
-  // const day1 =
 
-  console.log(prevWeek)
+componentDidMount() {
+  const prevWeek = getPreviousWeek()
+  this.setState({todaysDate: prevWeek[0], prevWeekDates: prevWeek})
 
-}
+  // console.log('today date', this.state.todaysDate)
+  // const todayImage = getImageByDate(this.state.todaysDate)
 
-async componentDidMount() {
-  this.createDateData()
-  const today = new Date()
-  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-  this.setState({ todaysDate: date })
-  // might not need ^
-  // console.log(this.state.todaysDate)
-  const todaysInfo = await getImageByDate('2020-09-16')
-  // console.log('today', todaysInfo)
-  const daysInfo = await getImageByDate('2020-01-28')
-  // console.log('1-28', daysInfo)
 }
   render() {
     return (
--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,8 @@
-export const getPreviousWeek = (date) => {
+export const getPreviousWeek = () => {
+  const date = new Date()
+  // const today = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate()
   let previousWeek = []
-  for (let i = 1; i < 7; i++) {
+  for (let i = 0; i < 7; i++) {
      // const day = new Date(date.getFullYear(), date.getMonth(), date.getDate() - i)
      const day = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+(date.getDate() - i)
      previousWeek.push(day)
