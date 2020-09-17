8798c670e031b4b2c427881bd9e9097df5fda50a
Jordy Shryock <jordy1611@gmail.com>
2020-09-17 10:56:06 -0700

    Adjust api calls

--- src/APICalls.js
+++ src/APICalls.js
@@ -13,7 +13,7 @@ import { apiKey } from './APIKey.js'
 // }
 
 export const getImageByDate = async (date) => {
-  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`)
+  const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_API_KEY}&date=${date}`)
   const data = await response.json()
 
   return data
