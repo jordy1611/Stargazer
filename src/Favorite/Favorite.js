47f58a8c02b2129876c70367a27b0ef52f7abf7f
Jordy Shryock <jordy1611@gmail.com>
2020-09-20 19:17:25 -0700

    Style spacing between favorites

--- src/Favorite/Favorite.js
+++ src/Favorite/Favorite.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Favorite.scss'
+import { Link } from 'react-router-dom'
 
 const Favorite = (props) => {
 
@@ -7,12 +8,13 @@ const Favorite = (props) => {
     <article className='favorite'>
       <div className='favorite-visible'>
         <p className='favorite-title'>{props.favorite.title}</p>
+        <p className='favorite-date'>{props.favorite.date}</p>
         <img src={props.favorite.hdurl} className='favorite-image' alt={`${props.favorite.title} image`}/>
       </div>
       <div className='favorite-hidden'>
         <p className='favorite-explanation'>{props.favorite.explanation}</p>
-        <p className='favorite-sdurl'>{props.favorite.url}</p>
-        <p className='favorite-hdurl'>{props.favorite.hdurl}</p>
+        <p className='favorite-sdurl'>sdurl: {props.favorite.url}</p>
+        <p className='favorite-hdurl'>hdurl: {props.favorite.hdurl}</p>
         { props.favorite.copyright &&
           <p className='favorite-copyright'>copyright: {props.favorite.copyright}</p>
         }
