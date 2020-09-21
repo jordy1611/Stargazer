11e6fd757239ed1b1fe86e440fe47df55bb7330b
Jordy Shryock <jordy1611@gmail.com>
2020-09-20 19:04:41 -0700

    Style favorite component

--- src/Favorite/Favorite.js
+++ src/Favorite/Favorite.js
@@ -5,16 +5,16 @@ const Favorite = (props) => {
 
   return(
     <article className='favorite'>
-      <div className='favorite-top'>
+      <div className='favorite-visible'>
         <p className='favorite-title'>{props.favorite.title}</p>
         <img src={props.favorite.hdurl} className='favorite-image' alt={`${props.favorite.title} image`}/>
       </div>
-      <div className='favorite-bottom'>
+      <div className='favorite-hidden'>
         <p className='favorite-explanation'>{props.favorite.explanation}</p>
-        <p className='favorite-sdurl'>{props.favorite.sdurl}</p>
+        <p className='favorite-sdurl'>{props.favorite.url}</p>
         <p className='favorite-hdurl'>{props.favorite.hdurl}</p>
         { props.favorite.copyright &&
-          <p className='favorite-copyright'>{props.favorite.copyright}</p>
+          <p className='favorite-copyright'>copyright: {props.favorite.copyright}</p>
         }
       </div>
     </article>
