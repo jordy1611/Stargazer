8d10005a5c1c08b49347c3529cccdd819b16d210
Jordy Shryock <jordy1611@gmail.com>
2020-09-21 18:50:40 -0700

    Add no props.favorite Favorite component render test

--- src/Favorite/Favorite.test.js
+++ src/Favorite/Favorite.test.js
@@ -31,4 +31,30 @@ describe('Favorite render', () => {
     expect(favSDUrl).toBeInTheDocument()
     expect(favCopyright).toBeInTheDocument()
   })
+
+  it('Should render no content when not favorite image is available', () => {
+    render(
+      <MemoryRouter>
+        <Favorite
+          favorite={{}}
+        />
+      </MemoryRouter>
+    )
+
+    const favTitle = screen.queryByText('Day1')
+    const favDate = screen.queryByText('2020-09-21')
+    const favImage = screen.queryByAltText('Day1 image')
+    const favExplanation = screen.queryByText('This is day1 of sampleWeek')
+    const favHDUrl = screen.queryByText('hdurl: https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_8504.jpg')
+    const favSDUrl = screen.queryByText('sdurl: https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_960.jpg')
+    const favCopyright = screen.queryByText('copyright: Juan Antonio Sendra')
+
+    expect(favTitle).not.toBeInTheDocument()
+    expect(favDate).not.toBeInTheDocument()
+    expect(favImage).not.toBeInTheDocument()
+    expect(favExplanation).not.toBeInTheDocument()
+    expect(favHDUrl).not.toBeInTheDocument()
+    expect(favSDUrl).not.toBeInTheDocument()
+    expect(favCopyright).not.toBeInTheDocument()
+  })
 })
