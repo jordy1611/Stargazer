c547d255137a018bbd22d32708c36203aced9d24
Jordy Shryock <jordy1611@gmail.com>
2020-09-21 17:54:11 -0700

    Finish and pass image page testing

--- src/testData.js
+++ src/testData.js
@@ -77,3 +77,34 @@ export const sampleWeek = [
 ]
 
 export const emptySampleWeek = [ {}, {}, {}, {}, {}, {}, {} ]
+
+export const sampleUserFavorites = [
+  {
+    copyright: "Juan Antonio Sendra",
+    date: "2020-09-21",
+    explanation: "This is day1 of sampleWeek",
+    hdurl: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_8504.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Day1",
+    url: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_960.jpg"
+  }, {
+    copyright: "Juan Antonio Sendra",
+    date: "2020-09-19",
+    explanation: "This is day3 of sampleWeek",
+    hdurl: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_8504.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Day3",
+    url: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_960.jpg"
+  }, {
+    copyright: "Juan Antonio Sendra",
+    date: "2020-09-15",
+    explanation: "This is day7 of sampleWeek",
+    hdurl: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_8504.jpg",
+    media_type: "image",
+    service_version: "v1",
+    title: "Day7",
+    url: "https://apod.nasa.gov/apod/image/2009/OmegaSunSail_Sendra_960.jpg"
+  }
+]
